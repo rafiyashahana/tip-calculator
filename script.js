f8ef831d6bba9f08d5172f9dbf9e3521cdbf470d
shahana <rafiyashahana@gmail.com>
2022-08-03 10:09:07 +0300

    added logic

--- script.js
+++ script.js
@@ -24,7 +24,23 @@ These are all the DIV ID's you're gonna need access to 👇
 
 // ** Calculate the total bill per person **
 
+let perPersonTotal = document.getElementById("perPersonTotal");
+const numberOfPeople = document.getElementById("numberOfPeople");
+const billTotalInput = document.getElementById("billTotalInput");
+const tipInput = document.getElementById("tipInput");
+
 const calculateBill = () => {
+  const billTotal = Number(billTotalInput.value);
+  const tipPercent = Number(tipInput.value);
+  let tipInputNumber = (tipPercent / 100) * billTotal;
+  let res = billTotal + tipInputNumber;
+
+  perPersonTotal.innerText = res;
+
+  let count = Number(numberOfPeople.innerText);
+  let total = res / count;
+  perPersonTotal.innerText = `$${total.toFixed(2)}`;
+
   // get bill from user input & convert it into a number
   // get the tip from user & convert it into a percentage (divide by 100)
   // get the total tip amount
@@ -35,6 +51,12 @@ const calculateBill = () => {
 
 // ** Splits the bill between more people **
 const increasePeople = () => {
+  let count = Number(numberOfPeople.innerText);
+  count++;
+  numberOfPeople.innerText = count;
+  console.log(count);
+  calculateBill();
+
   // increment the amount of people
   // update the DOM with the new number of people
   // calculate the bill based on the new number of people
@@ -42,6 +64,13 @@ const increasePeople = () => {
 
 // ** Splits the bill between fewer people **
 const decreasePeople = () => {
+  let count = numberOfPeople.innerText;
+  count > 1 && count--;
+  numberOfPeople.innerText = count;
+  console.log(count);
+
+  calculateBill();
+
   // guard clause
   // if amount is 1 or less simply return
   // (a.k.a you can't decrease the number of people to 0 or negative!)
